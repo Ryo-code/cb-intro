537fdcf42daef63c4d9f3bae9443db96f7ed1655
Ryo <kanadajinlee@yahoo.ca>
2016-11-03 17:01:03 +0000

    Just added a few lines on how it could be refactored

--- cb-args-foreachloop.js
+++ cb-args-foreachloop.js
@@ -11,3 +11,8 @@ function actionWhenFound(index) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+// Line 9~13 could be rewritten inline, like this:
+    // findWaldo(["Alice", "Bob", "Waldo", "Winston"], function(index) {
+    //   console.log("Found Waldo at index " + index + "!");
+    // });
